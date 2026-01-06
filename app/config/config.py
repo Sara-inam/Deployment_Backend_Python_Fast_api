90024c30de4e1b1dae7314db23cf5a266255730b
Sara-inam <sarainam60@gmail.com>
2026-01-06 16:04:20 +0500

    finally

--- app/config/config.py
+++ app/config/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 # JWT Settings
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "1080"))
 
 # Database
 DATABASE_URL = os.getenv("DATABASE_URL")
